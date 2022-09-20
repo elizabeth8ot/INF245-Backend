9014df414b6e663b416db91d1420ba3399b5c531
lloydcas <castillo.le@pucp.edu.pe>
2022-09-20 12:45:15 -0500

    Se ha agregado un nuevo mensaje

--- src/test/java/pe/edu/pucp/dovah/Inf245BackendApplicationTests.java
+++ src/test/java/pe/edu/pucp/dovah/Inf245BackendApplicationTests.java
@@ -1,13 +1,24 @@
 package pe.edu.pucp.dovah;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
 @SpringBootTest
+
 class Inf245BackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+}
+*/
+@SpringBootApplication
+public class Inf245BackendApplicationTests {
+    public static void main(String[] args){
+        SpringApplication.run(Inf245BackendApplicationTests.class,args);
+    }
 }
